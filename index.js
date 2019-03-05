ee3f99bd0886fee881a5174b5ec06a9480195ebb
mulan <mulan114@yahoo.com>
2019-03-05 16:36:11 -0500

    cleanup and documentation

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 const apikey='1';
 
 
+// since responseJson is different depending if the user has selected drink name or
+// ingredient to start, makesameformat puts the response in the same format prior
+// to being displayed
 function makesameformat(drinkName) {
-  console.log('in makesameformat');
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName.strDrink}`)
 	  .then(response => {
 	    if (response.ok) {
@@ -12,21 +14,16 @@ function makesameformat(drinkName) {
 	    }
 	    throw new Error(response.statusText);
 	  })
-	  .then(responseJson => {
-	    console.log(responseJson);
-	    displayRecipe(responseJson);
-	  })
+	  .then(responseJson => displayRecipe(responseJson))
       .catch(error => alert('Sorry, this cocktail is not in our database. Please try again.'))
 }
 
-
+// displays ingredients and instructions in the DOM
 function displayRecipe(recipe) {
-  console.log('displaying recipe in DOM')
   $('#options').addClass("hidden");
   $('#finalize').addClass("hidden");
   $('#cocktailrecipe').removeClass("hidden");
   recipe = recipe.drinks[0];
-  console.log(recipe.strIngredient1);
   $('#cocktailrecipe').append(
     `<h3>${recipe.strDrink}</h3>
     <br>
@@ -54,39 +51,34 @@ function displayRecipe(recipe) {
     );
 }
 
-
+// displays list of possible drinks that meet the name or ingredient criteria
+// selected by the user in the DOM
 function showOptions(options) {
-  console.log('showing options');
-  console.log(options);
-  console.log(options.drinks.length);
   $('#options').removeClass("hidden");
   $('#cocktailoptions').addClass("hidden");
   $('#ingredientoptions').addClass("hidden");
   $('#options-list').empty(); //empty the screen of old results
   for (let i=0; i<(options.drinks.length); i++) {
-    console.log(i);
     $('#options-list').append(
       `<input type="radio" name="whichcocktail" value=${i}> ${options.drinks[i].strDrink}<br>`)
   }
   $('#finalize').removeClass("hidden");
   $('#finalselect').click(event => {
-    console.log('finalizing selection');
     event.preventDefault();
     let selection = $('input[name="whichcocktail"]:checked').val();
-    console.log(selection);
     makesameformat(options.drinks[selection]);
   })
 }
 
+// retrieves the list of drinks that meet the drink name criteria selected
+// by the user via thecocktaildb.com api
 function getRecipes() {
   console.log('getting Recipe');
   $('#cocktailoptions').removeClass("hidden");
   $('#introduction').addClass("hidden");
   $('#cocktailsearch').click(event => {
-    console.log('getting input');
     event.preventDefault();
     let cocktail= $('input[name="cocktailname"]').val();
-    console.log(cocktail);
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
       .then(response => {
         if (response.ok) {
@@ -99,12 +91,13 @@ function getRecipes() {
   })
 }
 
+// retrieves the list of drinks that meet the ingredient criteria selected
+// by the user via thecocktaildb.com api
 function getList() {
   console.log('getting List');
   $('#ingredientoptions').removeClass("hidden");
   $('#introduction').addClass("hidden");
   $('#ingredientsearch').click(event => {
-    console.log('getting input');
     event.preventDefault();
     let ingredient= $('input[name="ingredientname"]').val();
     console.log(ingredient);
@@ -120,17 +113,16 @@ function getList() {
   })
 }
 
+// landing page for Mulan's Mixology, offering options to search the database 
+// using a cocktail name/keyword or by ingredient
 function watchForm() {
   $('#initialselect').click(event => {
-    console.log('selecting option');
     event.preventDefault();
-    console.log($('input[name="whichoption"]:checked').val());
     let option = $('input[name="whichoption"]:checked').val();
     if (option === "0") {
       console.log("goto getRecipes");
       getRecipes();
     } else if (option === "1") {
-      console.log("goto getList");
       getList();
     }
   })
